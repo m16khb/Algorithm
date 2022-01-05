6e14e2b85e4c1280217ac8b5e816de7a89560782
m16kh <m16kh@DESKTOP-I5S770O>
2022-01-05 22:40:50 +0900

    양수인지 음수인지 0인지 판단하는 알고리즘

---
+++ Algorithm/src/chapter01/JudgeSign.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+import java.util.Scanner;
+
+public class JudgeSign {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요 : ");
+		int n = s.nextInt();
+		
+		if(n > 0)
+			System.out.println("이 수는 양수입니다.");
+		else if(n < 0)
+			System.out.println("이 수는 음수입니다.");
+		else
+			System.out.println("이 수는 0입니다.");
+	}
+
+}
